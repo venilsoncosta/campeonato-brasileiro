0a1a614b10755b7ac795edf8878092b39a3c6785
Venilson Neves <103954028+venilson0708@users.noreply.github.com>
2024-11-22 15:37:44 -0300

    alterei o banco de dados utilizado. Agora o banco usado e o postgres

--- src/main/java/com/venilson/campeonato_brasileiro/CampeonatoBrasileiroApplication.java
+++ src/main/java/com/venilson/campeonato_brasileiro/CampeonatoBrasileiroApplication.java
@@ -14,21 +14,4 @@ public class CampeonatoBrasileiroApplication {
 		SpringApplication.run(CampeonatoBrasileiroApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner LoadData(ClubeRepository clubeRepository) {
-		return args -> {
-			Clube clube1 = new Clube(null, "São paulo", "Morumbi", 0, 0,
-					0, 0, 0, 0);
-
-			Clube clube2 = new Clube(null, "Flamengo", "Maracanã", 0, 0,
-					0, 0, 0, 0);
-
-			Clube clube3 = new Clube(null, "Corinthians", "Neo Quimica arena", 0, 0,
-					0, 0, 0, 0);
-
-			clubeRepository.save(clube1);
-			clubeRepository.save(clube2);
-			clubeRepository.save(clube3);
-		};
-	}
 }
